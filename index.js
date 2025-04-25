e357883997c2e505f3cca47215075419ce07727f
ak1606 <asitkorde16@gmail.com>
2025-04-25 19:57:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello World from Node.js!');
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
